95595ff48fd4697b8e7017a1a797f520b669d772
annimank <mankki.anni@gmail.com>
2023-10-01 18:21:22 +0300

    First

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet, Button, TextInput, Text } from 'react-native';
+import * as Speech from 'expo-speech';
+import * as Font from 'expo-font';
 
 export default function App() {
+  const [textToSpeak, setTextToSpeak] = React.useState('');
+  const [fontLoaded, setFontLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    async function loadCustomFont() {
+      await Font.loadAsync({
+        'Lobster-Regular': require('./assets/Lobster-Regular.ttf'),
+      });
+      setFontLoaded(true);
+    }
+
+    loadCustomFont();
+  }, []);
+
+  const speak = () => {
+    if (textToSpeak.trim() !== '') {
+      Speech.speak(textToSpeak);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {fontLoaded && (
+        <Text style={styles.title}>Text-to-Speech</Text>
+      )}
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="What do you want me to say?"
+          placeholderTextColor='#E2A2BB'
+          value={textToSpeak}
+          onChangeText={(text) => setTextToSpeak(text)}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          color="#E2A2BB"
+          title="SPEAK"
+          onPress={speak}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +52,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#581845',
+    padding: 20,
+  },
+  title: {
+    fontFamily: 'Lobster-Regular',
+    fontSize: 50,
+    color: '#E2A2BB',
+    marginBottom: 20,
+  },
+  inputContainer: {
+    width: '100%',
+    marginBottom: 20,
+    alignItems: 'center',
+  },
+  input: {
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    fontSize: 16,
+    padding: 10,
+    textAlign: 'center',
+    width: 260,
+    color: '#581845',
+  },
+  buttonContainer: {
+    width: '20%',
   },
 });
